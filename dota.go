61cc7cb671e70871e716b8346e16efdfa8df1ddc
Richard Ziegler <37094831+richardziegler@users.noreply.github.com>
2019-04-08 22:03:16 -0500

    Added CLI logo, can enter username now, working on Steam API Get - RZ

--- dota.go
+++ dota.go
@@ -1,9 +1,55 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+	"strings"
 )
 
+func getUserName() string {
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Print("Please enter your Steam Username: ")
+	username, _ := reader.ReadString('\n')
+	trimmedUsername := strings.TrimSuffix(username, "\n")
+	return trimmedUsername
+}
+
+func getSteamID(steamAPIKey string, username string) string {
+	steamAPIUrl := fmt.Sprintf("http://api.steampowered.com/ISteamUser/ResolveVanityURL/v0001/?key=%s&vanityurl=%d, steamAPIKey, username")
+
+	steamResponse, err := http.Get(steamAPIUrl)
+
+	if err != nil {
+		fmt.Print(err.Error())
+		os.Exit(1)
+	}
+
+	responseData, err := ioutil.ReadAll(steamResponse.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return string(responseData)
+}
+
 func main() {
-	fmt.Println("I love Dota!")
+
+	steamAPIKey := "SteamKeyHere"
+	// var openDotaAPIKey string = ""
+
+	fmt.Println("   ___  ____  _________     ___  ___  ____  __________   ____\n" +
+		"  / _ \\/ __ \\/_  __/ _ |   / _ \\/ _ \\/ __ \\/ __/  _/ /  / __/\n" +
+		" / // / /_/ / / / / __ |  / ___/ , _/ /_/ / _/_/ // /__/ _/  \n" +
+		"/____/\\____/ /_/ /_/ |_| /_/  /_/|_|\\____/_/ /___/____/___/  \n" +
+		"                                                             ")
+
+	myUsername := getUserName()
+
+	fmt.Println("I love Dota and my username is " + myUsername + "!")
+	fmt.Println(getSteamID(steamAPIKey, myUsername))
+
 }
