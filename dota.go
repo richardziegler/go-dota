b0537a499f2f9efcff016f27a7c038a7862880ca
Richard Ziegler <37094831+richardziegler@users.noreply.github.com>
2019-04-09 21:26:31 -0500

    Made progress on JSON parsing and successfully calling Steam API now. - RZ

--- dota.go
+++ dota.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bufio"
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"log"
@@ -10,35 +11,14 @@ import (
 	"strings"
 )
 
-func getUserName() string {
-	reader := bufio.NewReader(os.Stdin)
-	fmt.Print("Please enter your Steam Username: ")
-	username, _ := reader.ReadString('\n')
-	trimmedUsername := strings.TrimSuffix(username, "\n")
-	return trimmedUsername
-}
-
-func getSteamID(steamAPIKey string, username string) string {
-	steamAPIUrl := fmt.Sprintf("http://api.steampowered.com/ISteamUser/ResolveVanityURL/v0001/?key=%s&vanityurl=%d, steamAPIKey, username")
-
-	steamResponse, err := http.Get(steamAPIUrl)
-
-	if err != nil {
-		fmt.Print(err.Error())
-		os.Exit(1)
-	}
-
-	responseData, err := ioutil.ReadAll(steamResponse.Body)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	return string(responseData)
+type SteamAPIResult struct {
+	SteamID int `json:"steamid"`
+	Success int `json:"success"`
 }
 
 func main() {
 
-	steamAPIKey := "SteamKeyHere"
+	steamAPIKey := "APIKeyHere"
 	// var openDotaAPIKey string = ""
 
 	fmt.Println("   ___  ____  _________     ___  ___  ____  __________   ____\n" +
@@ -52,4 +32,37 @@ func main() {
 	fmt.Println("I love Dota and my username is " + myUsername + "!")
 	fmt.Println(getSteamID(steamAPIKey, myUsername))
 
+}
+
+func getUserName() string {
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Print("Please enter your Steam Username: ")
+	username, _ := reader.ReadString('\n')
+	trimmedUsername := strings.TrimSuffix(username, "\n")
+	return trimmedUsername
+}
+
+func getSteamID(steamAPIKey string, username string) int {
+	steamAPIUrl := fmt.Sprintf("http://api.steampowered.com/ISteamUser/ResolveVanityURL/v0001/?key=%s&vanityurl=%s", steamAPIKey, username)
+
+	steamResponse, err := http.Get(steamAPIUrl)
+	if err != nil {
+		fmt.Print(err.Error())
+		os.Exit(1)
+	}
+
+	responseData, err := ioutil.ReadAll(steamResponse.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var steamAPIResult SteamAPIResult
+
+	json.Unmarshal(responseData, &steamAPIResult)
+
+	fmt.Println("ID: " + string(steamAPIResult.SteamID))
+
+	response := steamAPIResult.SteamID
+
+	return response
 }
